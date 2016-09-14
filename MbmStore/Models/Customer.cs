d9c92a892d5c1931718b5dbd0af0db31abc9573e
WodeCraft <jcr@wodecraft.dk>
2016-09-14 16:52:54 +0200

    Initial commit

---
+++ MbmStore/Models/Customer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MbmStore.Models
+{
+    public class Customer
+    {
+        private List<string> phoneNumbers = new List<string>();
+        private DateTime birthDate;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        //public string Phonenumber { get; set; }
+
+        public List<string> PhoneNumbers
+        {
+            get
+            {
+                return phoneNumbers;
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+            set
+            {
+                birthDate = value;
+
+                int age = Age;
+                if (age < 0 && age > 120)
+                {
+                    throw new Exception("Age not accepted");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Now;
+                int age = now.Year - birthDate.Year;
+
+                if (now.Month < birthDate.Month
+                    || (now.Month == birthDate.Month && now.Day < birthDate.Day))
+                {
+                    age--;
+                }
+
+                return age;
+            }
+        }
+
+        public Customer(string firstName, string lastName, string address, string zip, string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Zip = zip;
+            City = city;
+        }
+
+        public void addPhone(string phone)
+        {
+            if (phoneNumbers == null)
+            {
+                phoneNumbers = new List<string>();
+            }
+            phoneNumbers.Add(phone);
+        }
+
+    }
+}
---
+++ MbmStore/Models/Movie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MbmStore.Models
+{
+    public class Movie
+    {
+        // fields
+        private string title;
+        private decimal price;
+        private string director;
+
+        // properties
+        public string Title
+        {
+            get { return title; } // read
+        }
+
+        public decimal Price
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Price is not accepted");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+            get { return price; }
+        }
+
+
+        public string ImageUrl { get; set; }
+
+        public string Director
+        {
+            get { return director; }
+        }
+
+
+        // constructors
+        public Movie(string title, decimal price)
+        {
+            this.title = title;
+            this.price = price;
+        }
+
+        public Movie(string title, decimal price, string imageUrl, string director)
+        {
+            this.title = title;
+            this.price = price;
+            ImageUrl = imageUrl;
+            this.director = director;
+        }
+    }
+}
